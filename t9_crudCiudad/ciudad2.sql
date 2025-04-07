07c166aca13a88f91feaeb9947aa12577d4b80ac
LUCIA <castaneralucia@gmail.com>
2025-04-07 12:49:55 +0200

    terminado y comentado

--- t9_crudCiudad/ciudad2.sql
+++ t9_crudCiudad/ciudad2.sql
@@ -26,10 +26,10 @@ DROP TABLE IF EXISTS `ciudad`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ciudad` (
   `codigo` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `numhabitantes` int DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,30 @@ CREATE TABLE `ciudad` (
 
 LOCK TABLES `ciudad` WRITE;
 /*!40000 ALTER TABLE `ciudad` DISABLE KEYS */;
+INSERT INTO `ciudad` VALUES (1,'madrid',100),(52,'peru',9),(152,'valencia',300),(153,'bilbao',150),(154,'nuevaYork',500);
 /*!40000 ALTER TABLE `ciudad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ciudad_SEQ`
+--
+
+DROP TABLE IF EXISTS `ciudad_SEQ`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ciudad_SEQ` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ciudad_SEQ`
+--
+
+LOCK TABLES `ciudad_SEQ` WRITE;
+/*!40000 ALTER TABLE `ciudad_SEQ` DISABLE KEYS */;
+INSERT INTO `ciudad_SEQ` VALUES (251);
+/*!40000 ALTER TABLE `ciudad_SEQ` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +73,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-04 10:39:32
+-- Dump completed on 2025-04-07 12:49:09
